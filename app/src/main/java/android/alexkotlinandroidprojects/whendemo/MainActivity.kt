7c096bf6036bcdc1602b5840b05c82490912d05c
Alexander Rau <alexanderrau86@yahoo.com>
2020-04-07 16:53:55 -0600

    Continue chapter 8

--- app/src/main/java/android/alexkotlinandroidprojects/whendemo/MainActivity.kt
+++ app/src/main/java/android/alexkotlinandroidprojects/whendemo/MainActivity.kt
@@ -2,11 +2,25 @@ package android.alexkotlinandroidprojects.whendemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val name:String = "Nile"
+        when(name) {
+            "Atlantic", "Pacific", "Arctic" ->
+                Log.i("Found:", "$name is an ocean")
+            "Thames", "Nile", "Mississippi" ->
+                Log.i("Found:", "$name is a river")
+            "Labrador", "Beagle", "JAck Russel" ->
+                Log.i("Found:", "$name is a dog")
+            else -> {
+                Log.i("Not found:", "$name is not in database")
+            }
+        }
     }
 }
